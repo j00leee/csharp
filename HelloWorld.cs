43c60cdf6e0b3bfb1bd4dbbe988daec46182a9b9
julie chung <julie.minjung.chung@gmail.com>
2021-10-25 14:04:09 -0400

    first commit

---
+++ HelloWorld.cs
@@ -0,0 +1,10 @@
+//Create a program to display "Hello World" using Console.Window
+//This is the first program of C#. I learned the basics of C# and how to display the output in the pop up window.
+
+using System;
+
+class HelloWorld{
+    public static void Main(){
+        System.Windows.Forms.MessageBoxShow("Hello World");
+    }
+}
---
+++ MyInfo.cs
@@ -0,0 +1,11 @@
+/*In this C# program, I learned that the escape character, "\n" allows us to write the Console.WriteLine
+in one line, but when it's executed, it's on the next line."
+*/
+
+using System;
+
+class MyInfo{
+    public static void Main(){
+        System.Windows.Forms.MessageBox.Show("Hello everyone!\nMy name is Julie.");
+    }
+}
