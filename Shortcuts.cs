27259721e9575a7b9accf256bb9f6c07293066cb
julie chung <julie.minjung.chung@gmail.com>
2021-11-19 23:44:04 -0500

    add shortcuts

---
+++ Shortcuts.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Diagnostics;
+
+class Shortcutes{
+    public static void Main(){
+        string task = "";
+        Console.WriteLine("Choose the user type you want to access from below: ");
+        Console.WriteLine(" 1 - Document");
+        Console.WriteLine(" 2 - Spreadsheet");
+        Console.WriteLine(" 3 - Presentation");
+        Console.WriteLine(" 4 - Web Browser");
+        Console.WriteLine(" 5 - Drawing");
+        Console.WriteLine(" 6 - Text editor");
+        task = Console.ReadLine();
+
+        if(task == "1"){
+            Process.Start("winword.exe");
+        }
+        else if(task == "2"){
+            Process.Start("excel.exe");
+        }
+        else if(task == "3"){
+            Process.Start("powerpnt.exe");
+        }
+        else if(task == "4"){
+            Console.WriteLine("Please select the browser type: ");
+            Console.WriteLine(" 1 - Google Chrome");
+            Console.WriteLine(" 2 - Microsoft Internet Explorer");
+            task = Console.ReadLine();
+            if(task == "1"){
+                Process.Start("chrome.exe");
+            }
+            else if(task -- "2"){
+                Process.Start("iexplore.exe");
+            }
+            else{
+                Console.WriteLine("Invalid");
+            }
+        }
+        else if(task == "5"){
+            Process.Start("mspaint.exe");
+        }
+        else if(task == "6"){
+            Process.Start("notepad.exe");
+        }
+        else{
+            Console.WriteLine("Invalid Entry");
+        }
+    }
+}
